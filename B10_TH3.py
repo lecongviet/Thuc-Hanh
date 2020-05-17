606d9deac4f4fedc116814a2588fceff8dd59a18
lecongviet <62605877+lecongviet@users.noreply.github.com>
2020-05-17 21:48:09 +0700

    TH_3

---
+++ B10_TH3.py
@@ -0,0 +1,11 @@
+import math
+def Tinh(R):
+    if R < 0:
+        print("Ban nhap khong hop le")
+    else:
+        CV = 2 * R * math.pi
+        DT = R * R * math.pi
+        print("Chu vi la :", CV)
+        print("Dien tich la :", DT)
+print("Tinh Chu Vi, Dien Tich Hinh Tron")
+Tinh(10)
